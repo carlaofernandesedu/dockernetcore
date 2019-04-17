226735d31bac1caa9b9ce2ab3ba398d399846440
carlos melo <carlaofernandesedu@gmail.com>
2019-04-17 15:30:56 -0300

    Implementacao simples para uso .net core e ef

---
+++ TodoApi/Repositorio/IAsyncCrudDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Repositorio
+{
+    public interface IAsyncCrudDAO<T>
+    {
+        void Create(T item);
+        Task<List<T>> Get();
+        Task<T> Find(int id);
+
+
+    }
+}
---
+++ TodoApi/Repositorio/TodoDAO.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Repositorio
+{
+    public class TodoDAO : IAsyncCrudDAO<TodoItem>
+    {
+        private readonly TodoContext _context;
+
+        public TodoDAO(TodoContext context)
+        {
+            _context =  context;
+        }
+
+        public void Create(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+            
+        }
+
+        public Task<TodoItem> Find(int id)
+        {
+            return _context.TodoItems.FindAsync(id);
+            
+        }
+
+        public Task<List<TodoItem>> Get()
+        {
+            return _context.TodoItems.ToListAsync();
+        }
+    }
+}
